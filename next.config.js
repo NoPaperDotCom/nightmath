8fe87a1b40c2b5dab9bf5131545e9ebcb0b36eb9
NoPaperDotCom <130741956+NoPaperDotCom@users.noreply.github.com>
2023-06-26 12:36:17 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const { i18n } = require("./next-i18next.config");
-const keys = require("./private.key.js");
+// const keys = require("./private.key.js");
 
 const nextConfig = {
   // reactStrictMode: true,
@@ -14,8 +14,8 @@ const nextConfig = {
     ADDRESS: "https://www.google.com/maps/place/%E9%A7%B1%E9%A7%9D%E6%BC%86%E5%A4%A7%E5%BB%88/@22.31056,114.22491,15z/data=!4m5!3m4!1s0x0:0xed282d5cf2908243!8m2!3d22.31056!4d114.22491",
     CORE_SERVER_URL: "https://parseapi.back4app.com",
     M1_SERVER_URL: "https://parseapi.back4app.com",
-    M2_SERVER_URL: "https://parseapi.back4app.com",
-    ...keys
+    M2_SERVER_URL: "https://parseapi.back4app.com"
+    // ...keys
   },
   i18n
 };
