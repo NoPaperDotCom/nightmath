0d60ad1f7db7902631539b43768d1052a2cba6e4
NoPaperDotCom <130741956+NoPaperDotCom@users.noreply.github.com>
2023-06-26 22:00:38 +0800

    Update index.js

--- utils/parse/index.js
+++ utils/parse/index.js
@@ -43,7 +43,7 @@ export const validUserSessionToken = async (course, sessionToken = "", abortCont
     console.log(sessionToken);
     const _response = await _fetch("/users/me", course, "GET", { "X-Parse-Session-Token": sessionToken }, {}, abortController);
     if (_response.status !== 200) {
-      console.log(_response.statusText);
+      console.log(_response);
       return new AppError({ text: "session-invalidation", status: _response.status, message: _response.statusText }); 
     }
 
@@ -51,6 +51,7 @@ export const validUserSessionToken = async (course, sessionToken = "", abortCont
     if (_user.code === 209) { return new AppError({ text: "session-invalidation", status: 209, message: _user.error }); }
     return { objectId: _user.objectId, name: _user.name, email: _user.email, expiredDate: _user.expiredDate, sessionToken }; 
   } catch (error) {
+    console.error(error);
     return new AppError({ message: error.message });
   }
 };
