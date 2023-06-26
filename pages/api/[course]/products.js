8c2456433749b64252b74739be9cb42ee89e92ff
NoPaperDotCom <130741956+NoPaperDotCom@users.noreply.github.com>
2023-06-26 23:48:44 +0800

    Add files via upload

---
+++ pages/api/[course]/products.js
@@ -0,0 +1,10 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { callMethod } from "@/utils/parse";
+
+export default async (req, res) => {
+  const _sessionToken = req.query.sessionToken;
+  const _course = req.query.course
+  const { status, user = { sessionToken: _sessionToken }, products = [] } = await callMethod(_course, "retrieveProducts", { sessionToken: _sessionToken });
+  console.log(status);
+  return res.status(200).json({ status, user, products });
+}
