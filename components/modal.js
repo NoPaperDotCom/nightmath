59dc00da7784ea06d1486eb2bbf4aadde27ee5f3
NoPaperDotCom <130741956+NoPaperDotCom@users.noreply.github.com>
2023-07-02 17:35:29 +0800

    Update modal.js

--- components/modal.js
+++ components/modal.js
@@ -64,7 +64,7 @@ export const IntroductionModal = ({ id = "", btnText = "", sessionToken, setSett
     <Modal id={id} title={_setting.title}>
       {(_setting.loading) ? (
         <Flex>
-          <Spin size={0.3} color={{ s: 0.5, l: 0.5 }} />
+          <Spin size={0.1} color={{ s: 0.5, l: 0.5 }} />
         </Flex>
       ) : ( 
         <>
